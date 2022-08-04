53c5447adb480661d6b01870fa11e279c5538636
Eno Dynowski <eno@dynowski.com>
2022-08-04 11:53:58 -0700

    Added comments

--- DayOfTheWeek.java
+++ DayOfTheWeek.java
@@ -4,6 +4,7 @@ public class DayOfTheWeek {
         System.out.println(dayOfWeekString(UserInput()));       
     }
 
+    //taking user input and returning the date as an array of integers
     public static int[] UserInput(){
         Scanner input = new Scanner(System.in);
         System.out.print("Enter month: ");
@@ -17,6 +18,7 @@ public class DayOfTheWeek {
         return date;
     }
 
+    //takes the 3 integers the user entered and converts to an in 0-6. 0 is Sunday, 1 is Monday, etc.
     public static int dayOfWeek(int month, int day, int year){
         int a = (14 - month) / 12;
         int y = year - a;
@@ -24,7 +26,8 @@ public class DayOfTheWeek {
         int d = (day + y + y/4 - y/100 + y/400 + (31*m)/12) % 7;
         return d;
     }
-
+    
+    //taking the user input, coverting to an int between 0 and 6, then converting that to a day of the week
     public static String dayOfWeekString(int[] date){
         int day = dayOfWeek(date[0], date[1], date[2]);
         String[] days = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
--- PigLatin.java
+++ PigLatin.java
@@ -1,17 +1,21 @@
 import java.util.Scanner;
 public class PigLatin {
     public static void main (String[] args) throws StringIndexOutOfBoundsException{
+        
+        //taking input from the user and storing it as a sentence
         String sentence = "";
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a sentence to translate:");
         sentence = input.nextLine();
         
-
+        //checking if the sentence is empty, and if it is exiting the program
         if (sentence.equals("")){
             System.exit(0);
 
         } else {
+            //running a loop to check if each sentence is empty or not. If it is, breaking and closing the program.
             while (!sentence.equals("")){
+                //translating the sentence
                 System.out.println(translateSentence(sentence));
                 System.out.print("Enter a sentence to translate:");
                 sentence = input.nextLine();
@@ -19,6 +23,8 @@ public class PigLatin {
         }
         input.close();
     }
+
+    //breaks the sentence into words, translates each word, and then rejoins the words together.
     public static String translateSentence(String sentence){
         String[] words = sentence.split(" ");
         String translatedSentence = "";
@@ -29,6 +35,7 @@ public class PigLatin {
     }
 
 
+    //translates individual words. Checking if the word starts with a vowel, or one of the combos of consonants/vowels that get moved together. 
     public static String translateOneWord(String word){
         
         if (startsWithVowel(word)){
@@ -44,11 +51,13 @@ public class PigLatin {
     }
 
 
+    //boolean to check if a word starts with a vowel
     public static boolean startsWithVowel(String word){
         String vowels = "aeiouAEIOU"; 
         return (vowels.indexOf(word.charAt(0)) != -1);
     }
 
+    //boolean to check if a word starts with a consonant combo
     public static boolean startsWithConsonantCombo(String word){
         String[] combos = {"bl", "br", "ch", "cl", "cr", "dr", "dw", "fl", "fr", "gl", "gr", "kl", "ph", "pl", "pr", "qu", "sc", "kn", "sk", "sl", "sm", "sn", "sp", "sq", "st", "sw", "th", "tr", "tw", "wh", "wr"};
         boolean startsWithCombo = false;
