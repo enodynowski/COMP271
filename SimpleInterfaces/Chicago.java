afd4ab27816b8a0ac957494fcbf58812483bb0f7
Eno Dynowski <eno@dynowski.com>
2022-10-24 14:40:47 -0500

    added test class, Chicago

---
+++ SimpleInterfaces/Chicago.java
@@ -0,0 +1,12 @@
+package SimpleInterfaces;
+
+public class Chicago {
+    public static void main(String[] args) {
+        City Chicago = new City("Chicago");
+        String test = Chicago.describeClimate();
+        System.out.println(test);
+        Chicago.la
+        Transportation.Chicago.setAirport("Ohare");
+    }
+    
+}
