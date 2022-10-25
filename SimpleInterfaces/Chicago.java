0585c6f270009dbf6e26d8e7f2e4e0f1805f76d2
Eno Dynowski <eno@dynowski.com>
2022-10-24 19:03:59 -0500

    Added Setters and Getters to everything

--- SimpleInterfaces/Chicago.java
+++ SimpleInterfaces/Chicago.java
@@ -2,11 +2,19 @@ package SimpleInterfaces;
 
 public class Chicago {
     public static void main(String[] args) {
-        City Chicago = new City("Chicago");
-        String test = Chicago.describeClimate();
-        System.out.println(test);
-        Chicago.la
-        Transportation.Chicago.setAirport("Ohare");
+        City Chicago = new City();
+        Chicago.setElevation(650);
+        Chicago.setLatitude(41.8781);
+        Chicago.setLongitude(87.6298);
+        Chicago.setAirport("Ohare").setBusService(true).setClimate("Arctic in the winter, nice in the summer").setHarbor("Montrose Harbor").setPop(2_700_000);
+
+        /*
+         * What I am unable to do, with the private variables in the top of City.java is this:
+         * Chicago.hasAirport = true;
+         * 
+         * If I call Chicago.hasAirport() this just returns whether or not Chicago has an airport. If I were to change
+         * the variables in City.java to public or protected, it would allow me to change their values. 
+         */
     }
     
 }
--- SimpleInterfaces/City.java
+++ SimpleInterfaces/City.java
@@ -2,6 +2,7 @@ package SimpleInterfaces;
 
 public class City extends Place implements Local, Statistical, Transportation {
 
+    //Assigning all the appropriate return values that will be used later on. 
     private String nameOfAirport;
     private boolean hasAirport;
     private String nameOfHarbor;
@@ -13,14 +14,34 @@ public class City extends Place implements Local, Statistical, Transportation {
     private int population;
     private String climateDescription;
 
+    /*
+     * All methods are either Setters or Getters. 
+     * Methods that start with setSomething are setters, and allow you to use this class elsewhere to create and set attributes
+     * of a city. E.g. setAirport()
+     * 
+     * All the other methods are Getters, meaning that they do nothing other than return a value, for use elsewhere. E.g. hasAirport() and nameOfAirport() 
+     */
 
+    /**
+     * Airport Setter
+     * @param airportName
+     * @return City, for method chaining. 
+     */
+    public City setAirport(String airportName){
+        if (airportName != null){
+            hasAirport = true;
+            nameOfAirport = airportName;
+        } else {
+            hasAirport = false;
+        }
+        return this;
+    }
     /**
      * @return boolean, true if city has airport
      */
     public boolean hasAirport() {
         return hasAirport;
     }
-
     /**
      * @return String name of airport
      */
@@ -29,12 +50,25 @@ public class City extends Place implements Local, Statistical, Transportation {
     }
 
     /**
+     * Harbor setter
+     * @param harborName
+     * @return City, for method chaining
+     */
+    public City setHarbor (String harborName){
+        if (harborName != null){
+            hasHarbor = true;
+            nameOfHarbor = harborName;
+        } else {
+            hasHarbor = false;
+        }
+        return this;
+    }
+    /**
      * @return boolean, true if city has harbor
      */
     public boolean hasHarbor() {
         return hasHarbor;
     }
-
     /**
      * @return String name of harbor
      */
@@ -43,12 +77,25 @@ public class City extends Place implements Local, Statistical, Transportation {
     }
 
     /**
+     * Train Station setter
+     * @param TrainStationName
+     * @return City, for method chaining
+     */
+    public City setTrainStation(String TrainStationName){
+        if (TrainStationName != null){
+            hasTrainStation = true;
+            nameOfTrainStation = TrainStationName;
+        } else {
+            hasTrainStation = false;
+        }
+        return this;
+    }
+    /**
      * @return boolean, true if city has train station
      */
     public boolean hasTrainStation() {
         return hasTrainStation;
     }
-
     /**
      * @return String with name of train station
      */
@@ -56,11 +103,31 @@ public class City extends Place implements Local, Statistical, Transportation {
         return nameOfTrainStation;
     }
 
+
+    /**
+     * Setter for Bus Service
+     * @param b
+     * @return City, for method chainig
+     */
+    public City setBusService(boolean b){
+        hasBusService = b;
+        return this;
+    }
     /**
      * @return boolean, true if the city has a bus service
      */
     public boolean hasBusService() {
         return hasBusService;
+    }
+
+    /**
+     * Subway Network Setter
+     * @param b boolean, sets hasSubwayNetwork to true. 
+     * @return City, to allow chaining of methods
+     */
+    public City setSubwayNetwork(boolean b){
+        hasSubwayNetwork = b;
+        return this;
     }
     /**
      * @return boolean, true if the city has a subway network
@@ -70,6 +137,15 @@ public class City extends Place implements Local, Statistical, Transportation {
     }
 
     /**
+     * setter for population
+     * @param pop
+     * @return City, for method chaining
+     */
+    public City setPop(int pop){
+        population = pop;
+        return this;
+    }
+    /**
      * @return an integer of the city's population
      */
     public int population() {
@@ -77,15 +153,18 @@ public class City extends Place implements Local, Statistical, Transportation {
     }
 
     /**
+     * Setter for the climate description. 
+     * @param climate
+     * @return City, for method chaining
+     */
+    public City setClimate(String climate){
+        climateDescription = climate;
+        return this;
+    }
+    /**
      * @return a string containing a description of the climate at the city. 
      */
     public String describeClimate() {
         return climateDescription;
     }
-
-    public void setAirport(String airportName){
-        hasAirport = true;
-        nameOfAirport = airportName;
-    }
-
 }  // class City
